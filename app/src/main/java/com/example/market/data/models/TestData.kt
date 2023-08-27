3e04bd7066270a5c31a4f238d5e69ce284d60513
Akmal <akmalrustamovich0406@gmail.com>
2023-08-27 02:48:48 -0700

    deleted two data class

--- app/src/main/java/com/example/market/data/models/TestData.kt
+++
@@ -1,5 +0,0 @@
-package com.example.market.data.models
-
-data class TestData(
-    val id:Int
-)
--- app/src/main/java/com/example/market/data/models/TestDataClassForGitHub.kt
+++
@@ -1,6 +0,0 @@
-package com.example.market.data.models
-
-data class TestDataClassForGitHub(
-    val name: String,
-    val surname: String
-)
